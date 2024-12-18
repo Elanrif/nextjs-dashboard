ea4f7345808146e78686bcc930aa2e996a17f353
Elanrif <saidbacoelanrif@gmail.com>
2024-12-18 17:11:21 +0100

    feat: chatbox

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,20 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 
 export default function Layout({children}:{children:React.ReactNode}) {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                <SideNav />
-            </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
         </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}{' '}
+          <script
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src="//js-na1.hs-scripts.com/48580043.js"
+          ></script>
+        </div>
+      </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}
+      <script 
+        type="text/javascript" 
+        id="hs-script-loader" 
+        async defer 
+        src="//js-na1.hs-scripts.com/48580043.js">
+      </script>
+    </body>
     </html>
   );
 }
